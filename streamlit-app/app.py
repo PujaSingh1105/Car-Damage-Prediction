f212bb65a95e63a16b6e461e9734446ee1ee6908
PujaSingh1105 <pujasingh46171@email.com>
2025-04-30 21:38:46 +0530

    Car Damage Prediction

---
+++ streamlit-app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from model_helper import predict
+st.title("Vehical Damage Detection")
+uploaded_file = st.file_uploader("Upload the File",type = ["jpg","png"])
+if uploaded_file:
+    image_path = "temp_file.jpg"
+    with open(image_path,"wb") as f:
+        f.write(uploaded_file.getbuffer())
+        st.image(uploaded_file, caption = "Uploaded File",use_container_width = True)
+        prediction = predict(image_path)
+        st.info(f"Predicted class is : {prediction}")
+
+
